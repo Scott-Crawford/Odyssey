d497d8855fb47e103eb42dad58942dfaddc977bc
Scott Crawford <sfc17@pitt.edu>
2017-12-16 04:25:34 -0500

    Initial commit

---
+++ app/src/main/java/com/pitt/android/odyssey/MainActivity.java
@@ -0,0 +1,362 @@
+package com.pitt.android.odyssey;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.Message;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.method.KeyListener;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.pitt.android.odyssey.R;
+
+import java.lang.ref.WeakReference;
+import java.util.Set;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class MainActivity extends AppCompatActivity {
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+    /**
+     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()) {
+                case UsbService.ACTION_USB_PERMISSION_GRANTED: // USB PERMISSION GRANTED
+                    Toast.makeText(context, "USB Ready", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_PERMISSION_NOT_GRANTED: // USB PERMISSION NOT GRANTED
+                    Toast.makeText(context, "USB Permission not granted", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_NO_USB: // NO USB CONNECTED
+                    Toast.makeText(context, "No USB connected", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_DISCONNECTED: // USB DISCONNECTED
+                    Toast.makeText(context, "USB disconnected", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_NOT_SUPPORTED: // USB NOT SUPPORTED
+                    Toast.makeText(context, "USB device not supported", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+    private static final int UI_ANIMATION_DELAY = 300;
+    private final Handler mHideHandler = new Handler();
+    private UsbService usbService;
+    private View mContentView;
+    private View player1View;
+    private View player2View;
+    private View ballView;
+    int p1V;
+    int p2V;
+    int p1H;
+    int p2H;
+
+    private MyHandler mHandler;
+    private final Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            // Delayed removal of status and navigation bar
+
+            // Note that some of these constants are new as of API 16 (Jelly Bean)
+            // and API 19 (KitKat). It is safe to use them, as they are inlined
+            // at compile-time and do nothing on earlier devices.
+            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+    };
+
+    private final Runnable mShowPart2Runnable = new Runnable() {
+        @Override
+        public void run() {
+            // Delayed display of UI elements
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.show();
+            }
+
+        }
+    };
+    private boolean mVisible;
+    private final Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            hide();
+        }
+    };
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
+
+    private final ServiceConnection usbConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName arg0, IBinder arg1) {
+            usbService = ((UsbService.UsbBinder) arg1).getService();
+            usbService.setHandler(mHandler);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            usbService = null;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mHandler = new MyHandler(this, this);
+        setContentView(R.layout.activity_fullscreen);
+
+        mVisible = true;
+
+        mContentView = findViewById(R.id.fullscreen_content);
+        player1View = findViewById(R.id.player1);
+        player1View.setX(200);
+        player1View.setY(300);
+        p1H=200;
+        p1V=300;
+
+        player2View = findViewById(R.id.player2);
+        player2View.setX(425);
+        player2View.setY(300);
+        p2H=425;
+        p2V=300;
+
+        ballView = findViewById(R.id.ball);
+        ballView.setX(350);
+        ballView.setY(275);
+        ballView.setVisibility(View.INVISIBLE);
+
+
+
+        // Set up the user interaction to manually show or hide the system UI.
+
+
+        // Upon interacting with UI controls, delay any scheduled hide()
+        // operations to prevent the jarring behavior of controls going away
+        // while interacting with the UI.
+
+
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setFilters();  // Start listening notifications from UsbService
+        startService(UsbService.class, usbConnection, null); // Start UsbService(if it was not started before) and Bind it
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(mUsbReceiver);
+        unbindService(usbConnection);
+    }
+
+    private void toggle() {
+        if (mVisible) {
+            hide();
+        } else {
+            //show();
+        }
+    }
+
+    private void hide() {
+        // Hide UI first
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+
+        mVisible = false;
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable);
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+    }
+
+    /*@SuppressLint("InlinedApi")
+    private void show() {
+        // Show the system bar
+        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+        mVisible = true;
+
+        // Schedule a runnable to display UI elements after a delay
+        mHideHandler.removeCallbacks(mHidePart2Runnable);
+        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
+    }*/
+
+    /**
+     * Schedules a call to hide() in [delay] milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    private void startService(Class<?> service, ServiceConnection serviceConnection, Bundle extras) {
+        if (!UsbService.SERVICE_CONNECTED) {
+            Intent startService = new Intent(this, service);
+            if (extras != null && !extras.isEmpty()) {
+                Set<String> keys = extras.keySet();
+                for (String key : keys) {
+                    String extra = extras.getString(key);
+                    startService.putExtra(key, extra);
+                }
+            }
+            startService(startService);
+        }
+        Intent bindingIntent = new Intent(this, service);
+        bindService(bindingIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private void setFilters() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_GRANTED);
+        filter.addAction(UsbService.ACTION_NO_USB);
+        filter.addAction(UsbService.ACTION_USB_DISCONNECTED);
+        filter.addAction(UsbService.ACTION_USB_NOT_SUPPORTED);
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_NOT_GRANTED);
+        registerReceiver(mUsbReceiver, filter);
+    }
+
+    private static class MyHandler extends Handler {
+        private final WeakReference<MainActivity> mActivity;
+        private final WeakReference<Context> mContextWeakReference;
+        String[] valueBunch = new String[8];
+        String valueString = null;
+        String[] values = new String[8];
+        Toast mToast = null;
+
+        public MyHandler(MainActivity activity, Context context) {
+            mActivity = new WeakReference<>(activity);
+            mContextWeakReference = new WeakReference<Context>(context);
+
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UsbService.MESSAGE_FROM_SERIAL_PORT:
+                    try {
+                        String data = (String) msg.obj;
+                        valueBunch = data.split("\\n");
+                        valueString = valueBunch[1];
+                        if(mToast!=null){
+                            mToast.cancel();
+                        }
+                        mToast=Toast.makeText(mContextWeakReference.get(), valueString, Toast.LENGTH_LONG);
+                        mToast.show();
+                        values = valueString.split(",");
+                        mActivity.get().movePlayers(values);
+                    }
+                    catch (Exception e){
+                        Toast.makeText(mContextWeakReference.get(), "exception", Toast.LENGTH_SHORT).show();
+                    }
+            }
+        }
+    }
+
+    public void movePlayers(String[] values){
+        try {
+            p2V = Integer.parseInt(values[4]);
+            p2H = Integer.parseInt(values[3]);
+            player2View.setX(p2H*2);
+            player2View.setY(p2V);
+
+            p1H = Integer.parseInt(values[1]);
+            p1V = Integer.parseInt(values[2]);
+            player1View.setX(p1H*2);
+            player1View.setY(p1V);
+        }
+        catch (Exception e) {
+            Toast.makeText(this, "Too far", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_R:
+                Toast.makeText(this, "Reset, Player 1", Toast.LENGTH_SHORT).show();
+                return true;
+            case KeyEvent.KEYCODE_LEFT_BRACKET:
+                Toast.makeText(this, "Previous Card", Toast.LENGTH_SHORT).show();
+                return true;
+            case KeyEvent.KEYCODE_RIGHT_BRACKET:
+                Toast.makeText(this, "Next Card", Toast.LENGTH_SHORT).show();
+                return true;
+            case KeyEvent.KEYCODE_T:
+                Toast.makeText(this, "Ball Speed Up", Toast.LENGTH_SHORT).show();
+                return true;
+            case KeyEvent.KEYCODE_Y:
+                Toast.makeText(this, "Ball Speed Down", Toast.LENGTH_SHORT).show();
+                return true;
+            case KeyEvent.KEYCODE_COMMA:
+                Toast.makeText(this, "Reset, Player 2", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                Toast.makeText(this, event.toString(), Toast.LENGTH_SHORT).show();
+                return true;
+        }
+    }
+}
